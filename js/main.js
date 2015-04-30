730e6b9b2bbd5c2279d2f8bcb79071207e79d8e5
Márton Borlay <marton.borlay@kriek.hu>
2015-04-30 15:12:05 +0200

    Playground dialog

--- js/main.js
+++ js/main.js
@@ -22,12 +22,42 @@ $(document).ready(function() {
 		$(".prettyprint").each(function() {
 			var innerText = $(this).text();
 			if (innerText.indexOf("$ curl") > -1) {
-				$(this).after("<div class='codeFooter'><button class='tryit'>Try it out</button></div>")
+				var footer = $("<div class='codeFooter'></div>");
+				var button = $("<button class='tryit'>Try it out</button>");
+				footer.append(button);
+				$(this).after(footer);
+				button.click(function() {
+					displayDialog(parseCURL(innerText));
+				});
 			}
 		});
 
 	}).fail(function(xhr, err) {
 		console.log(xhr, err);
+	});
+
+
+	$(".dialog .close, .cover").click(function() {
+		$(".dialog, .cover").hide();
+	});
+
+	$(".dialog .submit").click(function() {
+		$.ajax({
+			method: $(".dialog button.active").data("method"),
+			dataType: "JSON",
+			url: $(".dialog .url").val(),
+			data: $(".dialog .data").val(),
+			type: "application/json"
+		}).done(function(response) {
+			$(".dialog .response").text(JSON.stringify(response, null, "\t"));
+		}).fail(function(response) {
+			$(".dialog .response").text(JSON.stringify(JSON.parse(response.responseText), null, "\t"));
+		});
+	});
+
+	$(".dialog .methodButtons button").click(function() {
+		$(".dialog .methodButtons button").removeClass("active");
+		$(this).addClass("active");
 	});
 });
 
@@ -41,4 +71,38 @@ function getMyId(){
 	}
 
 	return id;
+}
+
+function parseCURL(text) {
+	var data = {
+		method: "GET",
+		url: "http://cantrip.kriekapps.com/avm81dfs9k9" + getMyId(),
+		data: ""
+	}
+	var fragments = text.split("\\\n");
+	for (var i = 0; i < fragments.length; i++) {
+		var fragment = fragments[i].trim();
+		if (fragment.indexOf("-X") > -1) {
+			data.method = fragment.split(" ")[1];
+		}
+		else if (fragment.indexOf("-d") > -1) {
+			data.data = fragment.replace("-d ", "").replace(/\'/g, "");
+		}
+		else {
+			data.url = fragment.replace("$ curl ", "").replace(/\"/g, "");
+		}
+	}
+	return data;
+}
+
+function displayDialog(data) {
+	$(".dialog .url").val(data.url);
+	$(".dialog .data").val(data.data);
+	$(".dialog .methodButtons button").removeClass("active");
+	$(".dialog .methodButtons button[data-method=" + data.method + "]").addClass("active");
+	$(".dialog .response").text("");
+	$(".dialog, .cover").show();
+	$(".dialog").css({
+		top: window.scrollY + 100
+	});
 }
